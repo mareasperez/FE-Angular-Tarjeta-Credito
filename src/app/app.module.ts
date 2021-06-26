c95feba13bd89feee9749b5b4caee24adb25dbae
Maury Areas <mareasperez7@gmail.com>
2021-06-26 16:42:40 -0600

    initial commmit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-credito.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TarjetaCreditoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
